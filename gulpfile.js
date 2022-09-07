17f75141af04f15b361b0b01f36de97338e362bc
yshrael <yshrael.pimentel153241@gmail.com>
2022-09-07 03:41:07 -0300

    build

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp')
+const { parallel, series, src, dest } = gulp
+
+const uglifycss = require('gulp-uglifycss')
+const uglify = require('gulp-uglify')
+const htmlmin = require('gulp-htmlmin')
+const concat = require('gulp-concat')
+const rename = require('gulp-rename')
+
+
+function js(callback) {
+    src('./src/**/*.js')
+        .pipe(uglify())
+        .pipe(rename('main.min.js'))
+        .pipe(gulp.dest('build'))
+        
+    return callback()
+}
+
+function css(callback) {
+    src('./src/**/*.css')
+        .pipe(uglifycss({ uglyComments: true}))
+        .pipe(concat('style.min.css'))
+        .pipe(dest('build'))
+
+    return callback()
+}
+
+function html(callback) {
+    src('./src/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+        .pipe(rename('index.min.html'))
+        .pipe(dest('build'))
+
+    return callback()
+}
+
+function img(callback) {
+    src('./imagens/**/*.svg')
+        .pipe(dest('build/imagens'))
+
+    return callback()
+}
+
+module.exports.default = series(html, js, css, img)
